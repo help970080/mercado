b6cc003919612b44c5926f1d62d388d78d5806e6
Leonardo Luna <consultorpremium764@gmail.com>
2025-09-28 21:28:39 -0600

    ya casi

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,12 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-// rutas de productos
+// GET /api/products
+router.get("/", (req, res) => {
+  res.json([
+    { id: 1, name: "Producto demo 1", price: 100 },
+    { id: 2, name: "Producto demo 2", price: 200 }
+  ]);
+});
 
 export default router;
